0855ebe5da85701c23c8c5ea3405c76d16db0d33
AmadeoO <amadeo.fgarcia@gmail.com>
2018-10-02 17:43:16 -0300

    creacion de la clase Direccion

---
+++ src/main/java/com/powersys/Alquila/models/Altura.java
@@ -0,0 +1,11 @@
+package com.powersys.Alquila.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor 
+@Data
+public class Altura {
+	private int latitud;
+	private int longitud;
+}
--- src/main/java/com/powersys/Alquila/models/Direccion.java
+++ src/main/java/com/powersys/Alquila/models/Direccion.java
@@ -8,9 +8,9 @@ import lombok.Data;
 @Data
 @Builder
 public class Direccion {
-	private String altura;
+	private String piso;
 	private String calle;
 	private String barrio;
 	private String parcela;
-	
+	private Altura altura;
 }
